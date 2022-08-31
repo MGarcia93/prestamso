95f84afe86b3b5fe9f3476a4377fdffde2711379
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2022-08-30 23:04:03 -0300

    change columns for dues

--- database/migrations/2022_08_29_233430_create_lendings_table.php
+++ database/migrations/2022_08_29_233430_create_lendings_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\LendingStatus;
 
 return new class extends Migration
 {
@@ -16,14 +17,14 @@ return new class extends Migration
         Schema::create('lendings', function (Blueprint $table) {
             $table->id();
             $table->date('date');
-
             $table->unsignedBigInteger('client_id');
             $table->unsignedBigInteger('lender_id');
-
             $table->float('amount_number');
             $table->string('amount_word', 120);
+            $table->float('dues_amount', places: 2);
             $table->tinyInteger('dues_quantity');
             $table->tinyInteger('dues_current')->default(1);
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
             $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
             $table->foreign('lender_id')->references('id')->on('users')->onDelete('cascade');
