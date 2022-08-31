8b697a95facd7802c02dc719b71120928acabb6e
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2022-08-30 23:04:46 -0300

    add casting and properties necesary para list

--- app/Models/Lending.php
+++ app/Models/Lending.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\LendingStatus;
+use Carbon\Carbon;
 
 class Lending extends Model
 {
     use HasFactory;
-    protected $fillable = ['date', 'lender_id', 'amount_number', 'amount_word', 'client_id', 'dues_current', 'dues_quantity'];
+    protected $fillable = ['date', 'lender_id', 'amount_number', 'amount_word', 'client_id', 'dues_current', 'dues_quantity', 'dues_amount', 'status'];
 
+
+    protected $casts = [
+        'status' => LendingStatus::class,
+        'date' => 'date:Y-m-d'
+    ];
     public function lender()
     {
         return $this->belongsTo(User::class);
@@ -17,5 +24,13 @@ class Lending extends Model
     public function client()
     {
         return $this->belongsTo(Client::class);
+    }
+    public function must(): float
+    {
+        return $this->amount_number - $this->dues_amount * ($this->dues_current - 1);
+    }
+    public function nextPayment()
+    {
+        return Carbon::parse($this->date)->addMonth($this->dues_current)->format('d-m-Y');
     }
 }
